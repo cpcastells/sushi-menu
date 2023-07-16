cfc775f24cbb0e251ee8f2f00c0b48e848386637
Carles <cpcastells@gmail.com>
2023-07-16 22:18:58 +0200

    Add ThemeProvider and GlobalStyle to main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { ThemeProvider } from "styled-components";
 import "./index.css";
+import { theme } from "./styles/theme/theme.ts";
+import { GlobalStyle } from "./styles/GlobalStyle/GlobalStyle.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
