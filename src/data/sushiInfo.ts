86064585320de5a644e11a3a88728c15e273757c
Carles <cpcastells@gmail.com>
2023-07-16 21:38:41 +0200

    Add sushi data and images

---
+++ src/data/sushiInfo.ts
@@ -0,0 +1,54 @@
+import sushi from "./types";
+
+const sushiData: sushi[] = [
+  {
+    name: "Uramaki Tiger",
+    ingredients:
+      "Inverted roll of tempura prawn and spicy cheese cream, topped with mango and flambéed foie gras.",
+    photoName: "",
+    price: 12,
+    soldOut: false,
+  },
+  {
+    name: "Uramaki Rainbow",
+    ingredients:
+      "Inverted roll of avocado and crispy onion wrapped in salmon, tuna, and avocado.",
+    photoName: "",
+    price: 11,
+    soldOut: false,
+  },
+  {
+    name: "Uramaki Spicy tuna",
+    ingredients:
+      "Inverted roll of spicy tuna, avocado, Japanese mayo, crispy onion, and sesame.",
+    photoName: "",
+    price: 11,
+    soldOut: false,
+  },
+  {
+    name: "Uramaki Spicy salmon",
+    ingredients:
+      "Inverted roll of spicy salmon, avocado, Japanese mayo, crispy onion, and sesame.",
+    photoName: "",
+    price: 10,
+    soldOut: false,
+  },
+  {
+    name: "Uramaki Ebi Furai",
+    ingredients:
+      "Inverted roll of prawn with a panko coating, avocado, port-infused onion, crispy onion, and sesame.",
+    photoName: "",
+    price: 11,
+    soldOut: true,
+  },
+  {
+    name: "Uramaki California",
+    ingredients:
+      "Inverted roll of surimi, avocado, and Japanese mayo, covered with masago and sesame.",
+    photoName: "",
+    price: 10,
+    soldOut: false,
+  },
+];
+
+export default sushiData;
